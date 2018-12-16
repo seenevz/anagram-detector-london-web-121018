d73eef44383ff78ef55fe4a6a62d2c3b5d658122
seenevz <serneves@hotmail.com>
2018-12-16 17:37:43 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(compare_a)
+    compare_a.map do |compare_w|
+      if compare_w.split(//).sort == @word.split(//).sort
+        compare_w
+      end
+    end.compact
+  end
+end
